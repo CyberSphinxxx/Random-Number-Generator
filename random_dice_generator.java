4dcea242ecaffa9fdf59ff32eed8c0679872e01b
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-08-31 22:32:45 +0800

    Limits to 6 numbers

---
+++ random_dice_generator.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class random_dice_generator {
+    
+    public static void main (String[] args){
+
+        while (true) { 
+            Random random = new Random();
+
+            int x = random.nextInt(6) + 1;
+    
+            System.out.print(x);
+        }
+    }
+}
